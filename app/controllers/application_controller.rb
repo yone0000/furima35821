7812967aa8581ada43c379fc2874b6e186d699d8
yone0000 <ytytyt.0108@gamil.com>
2021-06-03 15:24:15 +0900

    3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,12 @@
-class ApplicationController < ActionController::Base
-
   class ApplicationController < ActionController::Base
     before_action :basic_auth
   
     private
     def basic_auth
       authenticate_or_request_with_http_basic do |username, password|
-        username == 35821 && password == 35821
+        username == '35821' && password == '35821'
       end
     end
   end
 
-end
+
