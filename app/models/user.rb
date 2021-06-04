cb22e66efefbe295cedfceef48ef3794dff5ee4d
yone0000 <ytytyt.0108@gamil.com>
2021-06-04 18:47:37 +0900

    conflict 修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-<<<<<<< Updated upstream
 
          validates :nickname, presence: true
          validates :last_name, presence: true
@@ -11,6 +10,4 @@ class User < ApplicationRecord
          validates :last_name_kana, presence: true
          validates :first_name_kana, presence: true
          validates :birth_date, presence: true
-=======
->>>>>>> Stashed changes
 end
