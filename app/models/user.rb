4efadcacb067f6ce2c9a1fd82c4d34f580d276b6
yone0000 <ytytyt.0108@gamil.com>
2021-06-06 16:29:13 +0900

    8 commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,14 @@ class User < ApplicationRecord
        with_options presence: true do
          validates :nickname
          validates :birth_date
+         with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "Full-width characters"} do
+              validates :first_name
+              validates :last_name
        end
-       with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "Full-width characters"}, presence: true do
-         validates :first_name
-         validates :last_name
+       with_options format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."} do
+              validates :first_name_kana
+              validates :last_name_kana
        end
-       with_options format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}, presence: true do
-         validates :first_name_kana
-         validates :last_name_kana
        end
           
    
