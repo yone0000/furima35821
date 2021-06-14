02f88101d1256eaf2fb4ebd7c07a5346696c4579
yone0000 <ytytyt.0108@gamil.com>
2021-06-14 12:55:05 +0900

    fist commit

--- app/models/schedule_delivery.rb
+++ app/models/schedule_delivery.rb
@@ -1,25 +1,9 @@
 class ScheduleDelivery < ActiveHash::Base
     self.data = [
       { id: 1, name: '--' },
-      { id: 2, name: 'メンズ' },
-      { id: 3, name: 'レディース' },
-      { id: 4, name: 'ベビー' },
-      { id: 5, name: 'キッズ' },
-      { id: 6, name: 'インテリア' },
-      { id: 7, name: '住まい' },
-      { id: 8, name: '小物' },
-      { id: 9, name: '本' },
-      { id: 10, name: '音楽・ゲーム' },
-      { id: 11, name: 'おもちゃ' },
-      { id: 12, name: 'ホビー' },
-      { id: 13, name: 'グッズ' },
-      { id: 14, name: '家電' },
-      { id: 15, name: 'スマホ' },
-      { id: 16, name: 'カメラ' },
-      { id: 17, name: 'スポーツ' },
-      { id: 18, name: 'レジャー' },
-      { id: 19, name: 'ハンドメイド' },
-      { id: 20, name: 'その他' }
+      { id: 2, name: '1~2日で発送' },
+      { id: 3, name: '2~3日で発送' },
+      { id: 4, name: '4~7日で発送' },
     ]
 
     include ActiveHash::Associations
